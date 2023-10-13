510d0c5ae83a6bcfc553c2f330696845522c232d
akbaroke <akbarmuhammad833@gmail.com>
2023-10-13 21:26:31 +0700

    refactor(route): make clean code

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,50 +6,63 @@ import Home from '@/pages/Home';
 import Personal from '@/pages/Personal';
 import Profile from '@/pages/Profile';
 import GuestMiddleware from '@/middlewares/GuestMiddleware';
+import NewPersonal from '@/pages/NewPersonal';
+
+type DataRouteType = {
+  path: string;
+  element: React.ReactElement;
+  middleware: 'guest' | 'user';
+};
 
 export default function Root() {
+  const dataRoute: DataRouteType[] = [
+    {
+      path: '/signin',
+      element: <Signin />,
+      middleware: 'guest',
+    },
+    {
+      path: '/signup',
+      element: <Signup />,
+      middleware: 'guest',
+    },
+    {
+      path: '/',
+      element: <Home />,
+      middleware: 'user',
+    },
+    {
+      path: '/profile',
+      element: <Profile />,
+      middleware: 'user',
+    },
+    {
+      path: '/personal',
+      element: <NewPersonal />,
+      middleware: 'user',
+    },
+    {
+      path: '/personal/:id',
+      element: <Personal />,
+      middleware: 'user',
+    },
+  ];
+
   return (
     <Routes>
-      <Route
-        path="/signin"
-        element={
-          <GuestMiddleware>
-            <Signin />
-          </GuestMiddleware>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <GuestMiddleware>
-            <Signup />
-          </GuestMiddleware>
-        }
-      />
-      <Route
-        path="/personal/:id"
-        element={
-          <UserMiddleware>
-            <Personal />
-          </UserMiddleware>
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-          <UserMiddleware>
-            <Profile />
-          </UserMiddleware>
-        }
-      />
-      <Route
-        path="/"
-        element={
-          <UserMiddleware>
-            <Home />
-          </UserMiddleware>
-        }
-      />
+      {dataRoute.map((route, index) => (
+        <Route
+          key={index}
+          path={route.path}
+          element={
+            route.middleware === 'guest' ? (
+              <GuestMiddleware>{route.element}</GuestMiddleware>
+            ) : (
+              <UserMiddleware>{route.element}</UserMiddleware>
+            )
+          }
+        />
+      ))}
     </Routes>
   );
 }
