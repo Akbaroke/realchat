a0a0fe729cd93abf8d2421db194e5ee53eb94a77
akbaroke <akbarmuhammad833@gmail.com>
2023-10-22 20:48:51 +0700

    refactor(assets): add and update

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,13 @@
 import DEFAULT_FOTO from './default_foto.png';
-import BLACK_OPENAI from './logo-openai.png';
-import WHITE_OPENAI from './bg-openai.png';
+import BLACK_OPENAI from './black-openai.png';
+import WHITE_OPENAI from './white-openai.png';
+import LOGO_REALCHAT from './logo-realchat.svg';
+import LOGOTEXT_REALCHAT from './logotext-realchat.svg';
 
-export { DEFAULT_FOTO, BLACK_OPENAI, WHITE_OPENAI };
+export {
+  DEFAULT_FOTO,
+  BLACK_OPENAI,
+  WHITE_OPENAI,
+  LOGO_REALCHAT,
+  LOGOTEXT_REALCHAT,
+};
