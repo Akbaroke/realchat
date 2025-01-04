3ff053ce26dccf3c54272a60e638b251c232f706
Akbaroke <akbarmuhammad833@gmail.com>
2025-01-04 21:09:48 +0700

    [update] update sticky in page new personal

--- src/pages/NewPersonal.tsx
+++ src/pages/NewPersonal.tsx
@@ -1,29 +1,29 @@
-import UserSearch from '@/components/atoms/UserSearch';
-import { useEffect, useState } from 'react';
-import { FiChevronLeft } from 'react-icons/fi';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { useNavigate } from 'react-router-dom';
-import { motion as mo } from 'framer-motion';
-import { v4 as uuidv4 } from 'uuid';
-import { useDebouncedState } from '@mantine/hooks';
-import getUserDatas, { UserInterface } from '@/services/getUsers';
-import { Loader, Transition } from '@mantine/core';
-import { useDispatch, useSelector } from 'react-redux';
-import { PersonalState, setPersonal } from '@/store/slices/personalSlice';
-import { DEFAULT_FOTO } from '@/assets';
-import { RootState } from '@/store';
+import UserSearch from "@/components/atoms/UserSearch";
+import { useEffect, useState } from "react";
+import { FiChevronLeft } from "react-icons/fi";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useNavigate } from "react-router-dom";
+import { motion as mo } from "framer-motion";
+import { v4 as uuidv4 } from "uuid";
+import { useDebouncedState } from "@mantine/hooks";
+import getUserDatas, { UserInterface } from "@/services/getUsers";
+import { Loader, Transition } from "@mantine/core";
+import { useDispatch, useSelector } from "react-redux";
+import { PersonalState, setPersonal } from "@/store/slices/personalSlice";
+import { DEFAULT_FOTO } from "@/assets";
+import { RootState } from "@/store";
 
 export default function NewPersonal() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const auth = useSelector((state: RootState) => state.auth);
   const { roomsPersonal } = useSelector((state: RootState) => state.rooms);
-  const [value, setValue] = useDebouncedState('', 1000);
+  const [value, setValue] = useDebouncedState("", 1000);
   const [listUsers, setListUsers] = useState<UserInterface[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (value.trim() !== '' && listUsers.length === 0) {
+    if (value.trim() !== "" && listUsers.length === 0) {
       setIsLoading(true);
       getUserDatas()
         .then((res) => setListUsers(res))
@@ -54,10 +54,11 @@ export default function NewPersonal() {
 
   return (
     <div className="flex flex-col">
-      <div className="flex items-center px-5 py-3 border-b sticky top-0 gap-5">
+      <div className="sticky top-0 z-50 flex items-center gap-5 px-5 py-3 bg-white border-b">
         <div
-          className="p-2 rounded-md border w-max text-gray-500 cursor-pointer hover:text-black transition-all"
-          onClick={() => navigate('/')}>
+          className="p-2 text-gray-500 transition-all border rounded-md cursor-pointer w-max hover:text-black"
+          onClick={() => navigate("/")}
+        >
           <FiChevronLeft size={16} />
         </div>
         <h1 className="font-medium">New Personal</h1>
@@ -69,17 +70,19 @@ export default function NewPersonal() {
         mounted={!!value}
         transition="slide-up"
         duration={400}
-        timingFunction="ease">
+        timingFunction="ease"
+      >
         {(styles) => (
-          <div className="px-5 bg-white z-10 relative" style={styles}>
-            <h1 className="font-medium my-1">Search Result</h1>
+          <div className="relative z-10 px-5 bg-white" style={styles}>
+            <h1 className="my-1 font-medium">Search Result</h1>
             <div className="flex flex-col gap-1">
               {filteredUsers.map((user, index) => (
                 <mo.div
-                  initial={{ opacity: 0, transform: 'translateY(50px)' }}
-                  animate={{ opacity: 1, transform: 'translateY(0px)' }}
+                  initial={{ opacity: 0, transform: "translateY(50px)" }}
+                  animate={{ opacity: 1, transform: "translateY(0px)" }}
                   transition={{ delay: index * 0.3, duration: 0.3 }}
-                  key={index}>
+                  key={index}
+                >
                   <div
                     onClick={() =>
                       handleDirectToPersonalRoom({
@@ -88,7 +91,8 @@ export default function NewPersonal() {
                         personal_id: uuidv4(),
                       })
                     }
-                    className="border-b border-gray-100 py-4 flex gap-3 cursor-pointer">
+                    className="flex gap-3 py-4 border-b border-gray-100 cursor-pointer"
+                  >
                     <LazyLoadImage
                       alt="foto"
                       effect="blur"
@@ -98,7 +102,7 @@ export default function NewPersonal() {
                       referrerPolicy="no-referrer"
                       className="rounded-lg"
                     />
-                    <div className="grid grid-cols-3 w-full">
+                    <div className="grid w-full grid-cols-3">
                       <div className="flex flex-col col-span-2">
                         <h1 className="text-[15px] font-medium">{user.name}</h1>
                         <p className="whitespace-nowrap w-full text-[13px] overflow-hidden overflow-ellipsis text-gray-400">
@@ -122,9 +126,10 @@ export default function NewPersonal() {
         mounted={isLoading}
         transition="slide-up"
         duration={400}
-        timingFunction="ease">
+        timingFunction="ease"
+      >
         {(styles) => (
-          <div className="m-auto my-10 z-0" style={styles}>
+          <div className="z-0 m-auto my-10" style={styles}>
             <Loader color="dark" size="sm" variant="dots" />
           </div>
         )}
